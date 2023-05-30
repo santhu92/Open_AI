556a16fc17c3a5443622fb846b6569ce04888627
santhu92 <123493413+santhu92@users.noreply.github.com>
2023-05-30 19:33:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
-api_key = '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'
-Serpa_api = '413b76062ffa99ea136621e3dfea021d6dbc0143cc576018bcbf2a399bd0ac36'
+api_key = '<your Key>'
+Serpa_api = '<Your Key>'
